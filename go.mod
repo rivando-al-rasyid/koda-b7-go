e346edcb9719efe037a051d4da03053175deac16
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-27 11:44:29 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rivando-al-rasyid/koda-b7-go
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+func circumference(r int) {
+	const circumference = 2 * math.Pi * r
+	return circumference
+}
+func circleArea(r int) {
+	const circleArea = 2 * math.Pi * r
+	return
+}
