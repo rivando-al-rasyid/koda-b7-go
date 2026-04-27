e5571dcf5d5a117ef744a67d23798c1e98683bb2
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-27 16:14:59 +0700

    feat : struct

--- main.go
+++ main.go
@@ -27,22 +27,49 @@ func printCircleResult(r float32) {
 
 }
 
+type dataSekolah struct {
+	Nama    string
+	Jurusan string
+}
+type dataDiri struct {
+	Nama        string
+	Foto        string
+	Email       string
+	Umur        uint8
+	NoTel       string
+	SPernikahan bool
+	Pendidikan  []dataSekolah
+}
+
 func main() {
-	var radius float32
-	var base int
+	// var radius float32
+	// var base int
 
-	fmt.Print("Enter a radius circle: ")
-	fmt.Scan(&radius) // Reads input and stores it in radius
-	printCircleResult(radius)
-	fmt.Print("Enter a base of right-angled triangle : ")
-	fmt.Scan(&base) // Reads input and stores it in base
-	rightAngleTriangle(base)
+	// fmt.Print("Enter a radius circle: ")
+	// fmt.Scan(&radius) // Reads input and stores it in radius
+	// printCircleResult(radius)
+	// fmt.Print("Enter a base of right-angled triangle : ")
+	// fmt.Scan(&base) // Reads input and stores it in base
+	// rightAngleTriangle(base)
 
-	numbers := []int{50, 75, 66, 20, 32, 90}
-	front := numbers[0 : len(numbers)-3]
-	end := numbers[len(numbers)-3:]
-	result := append(front, 88)
+	// numbers := []int{50, 75, 66, 20, 32, 90}
+	// front := numbers[0 : len(numbers)-3]
+	// end := numbers[len(numbers)-3:]
+	// result := append(front, 88)
+	// result = append(result, end...)
 
-	result = append(result, end...)
-
+	user := dataDiri{
+		Nama:        "Alex Riva",
+		Foto:        "profile.jpg",
+		Email:       "alex@example.com",
+		Umur:        25,
+		NoTel:       "08123456789",
+		SPernikahan: false,
+		Pendidikan: []dataSekolah{
+			{Nama: "SMA Negeri 1", Jurusan: "IPA"},
+			{Nama: "Universitas Indonesia", Jurusan: "Teknik Informatika"},
+		},
+	}
+	fmt.Println("User Name:", user.Nama)
+	fmt.Println("Major at last school:", user.Pendidikan[1].Jurusan)
 }
